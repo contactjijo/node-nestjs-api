b8af1f203ebd0ad2a4e4ce97b7ec8937f45a93af
Jijo Thomas <contactjijo@gmail.com>
2021-06-01 22:36:54 +0530

    configs updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB_CONNECTION_URI } from './config/configs';
 import { ProductsModule } from './products/products.module';
-
 @Module({
     imports: [
         ProductsModule,
         MongooseModule.forRoot(
-            'mongodb+srv://demo-user:7d7H1ZOyktYg6J50@cluster0.3fnsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            DB_CONNECTION_URI,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             }
-        ),
-        ConfigModule.forRoot({
-            envFilePath: '.env',
-        }),
+        )
     ],
     controllers: [AppController],
     providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './config/configs';
 
 async function bootstrap() {
+    // console.log('Process Environment Details =>', process.env);
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
